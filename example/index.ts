7a9d14706f38cd80a73856215d77dba3d0fbdb3c
saltyaom <saltyaom@gmail.com>
2025-07-31 19:57:55 +0700

    :broom: chore: ws

--- example/index.ts
+++ example/index.ts
@@ -9,11 +9,19 @@ const app = new Elysia({
 })
 	.use(cors())
 	.use(swagger())
+	.ws('/ws', {
+		open() {
+			console.log('OPENED')
+		},
+		message(ws, message) {
+			ws.send(message)
+		}
+	})
 	.get('/image', async () => file('test/images/midori.png'))
 	.get('/generator', async function* () {
 		for (let i = 0; i < 100; i++) {
-			await new Promise(resolve => setTimeout(resolve, 10))
-			yield "A"
+			await new Promise((resolve) => setTimeout(resolve, 10))
+			yield 'A'
 		}
 	})
 	.post('/', ({ body }) => body, {
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,253 @@
-import type { ElysiaAdapter } from 'elysia'
+import {
+	getSchemaValidator,
+	serializeCookie,
+	ValidationError,
+	type ElysiaAdapter
+} from 'elysia'
 import { WebStandardAdapter } from 'elysia/adapter/web-standard'
 
-import { serve, FastResponse } from 'srvx'
+import { defineHooks } from 'crossws'
+import { serve } from 'crossws/server'
+import { FastResponse } from 'srvx'
 
-import { isNumericString, randomId } from 'elysia/utils'
+import type {
+	Server as NodeServer,
+	IncomingMessage,
+	ServerResponse
+} from 'http'
+import type {
+	Http2Server,
+	Http2ServerRequest,
+	Http2ServerResponse
+} from 'http2'
+
+import { isNotEmpty, isNumericString, randomId } from 'elysia/utils'
 import type { Server } from 'elysia/universal'
+import {
+	createHandleWSResponse,
+	createWSMessageParser,
+	ElysiaWS
+} from 'elysia/ws'
+import { ServerWebSocket } from 'elysia/ws/bun'
+import { parseSetCookies } from 'elysia/adapter/utils'
+
+const toServerWebSocket = (ws: ServerWebSocket) => {
+	// @ts-ignore
+	ws.data = ws.context
+	ws.sendText = ws.send
+	ws.sendBinary = ws.send
+	ws.publishText = ws.publish
+	ws.publishBinary = ws.publish
+	ws.isSubscribed = () => false
+	// @ts-ignore
+	ws.cork = () => {}
+}
 
 export const node = () => {
+	const wsState: Record<string, unknown> = {}
+
 	return {
 		...WebStandardAdapter,
 		name: 'node',
+		ws(app, path, options) {
+			const { parse, body, response, ...rest } = options
+
+			const validateMessage = getSchemaValidator(body, {
+				// @ts-expect-error private property
+				modules: app.definitions.typebox,
+				// @ts-expect-error private property
+				models: app.definitions.type as Record<string, TSchema>,
+				normalize: app.config.normalize
+			})
+
+			const validateResponse = getSchemaValidator(response as any, {
+				// @ts-expect-error private property
+				modules: app.definitions.typebox,
+				// @ts-expect-error private property
+				models: app.definitions.type as Record<string, TSchema>,
+				normalize: app.config.normalize
+			})
+
+			app.route(
+				'WS',
+				path as any,
+				async (context: any) => {
+					// ! Enable static code analysis just in case resolveUnknownFunction doesn't work, do not remove
+					// eslint-disable-next-line @typescript-eslint/no-unused-vars
+					const { set, path, qi, headers, query, params } = context
+
+					const id = context.request.id
+
+					// @ts-ignore
+					context.validator = validateResponse
+
+					if (options.upgrade) {
+						if (typeof options.upgrade === 'function') {
+							const temp = options.upgrade(context as any)
+							if (temp instanceof Promise) await temp
+						} else if (options.upgrade)
+							Object.assign(
+								set.headers,
+								options.upgrade as Record<string, any>
+							)
+					}
+
+					if (set.cookie && isNotEmpty(set.cookie)) {
+						const cookie = serializeCookie(set.cookie)
+
+						if (cookie) set.headers['set-cookie'] = cookie
+					}
+
+					if (
+						set.headers['set-cookie'] &&
+						Array.isArray(set.headers['set-cookie'])
+					)
+						set.headers = parseSetCookies(
+							new Headers(set.headers as any) as Headers,
+							set.headers['set-cookie']
+						) as any
+
+					const handleResponse =
+						createHandleWSResponse(validateResponse)
+					const parseMessage = createWSMessageParser(parse)
+
+					let _id: string | undefined
+
+					if (typeof options.beforeHandle === 'function') {
+						const result = options.beforeHandle(context)
+						if (result instanceof Promise) await result
+					}
+
+					const errorHandlers = [
+						...(options.error
+							? Array.isArray(options.error)
+								? options.error
+								: [options.error]
+							: []),
+						...(app.event.error ?? []).map((x) =>
+							typeof x === 'function' ? x : x.fn
+						)
+					].filter((x) => x)
+
+					const handleErrors = !errorHandlers.length
+						? () => {}
+						: async (ws: ServerWebSocket<any>, error: unknown) => {
+								for (const handleError of errorHandlers) {
+									let response = handleError(
+										Object.assign(context, { error })
+									)
+									if (response instanceof Promise)
+										response = await response
+
+									await handleResponse(ws, response)
+
+									if (response) break
+								}
+							}
+
+					wsState[id] = {
+						context,
+						headers: isNotEmpty(set.headers)
+							? (set.headers as Record<string, string>)
+							: undefined,
+						id,
+						validator: validateResponse,
+						ping(data?: unknown) {
+							options.ping?.(data)
+						},
+						pong(data?: unknown) {
+							options.pong?.(data)
+						},
+						open: async (ws: ServerWebSocket<any>) => {
+							toServerWebSocket(ws)
+
+							try {
+								await handleResponse(
+									ws,
+									options.open?.(
+										new ElysiaWS(ws, context as any)
+									)
+								)
+							} catch (error) {
+								console.log(error)
+								handleErrors(ws, error)
+							}
+						},
+						message: async (
+							ws: ServerWebSocket<any>,
+							_message: any
+						) => {
+							const message = await parseMessage(
+								ws,
+								_message.text()
+							)
+
+							if (validateMessage?.Check(message) === false)
+								return void ws.send(
+									new ValidationError(
+										'message',
+										validateMessage,
+										message
+									).message as string
+								)
+
+							try {
+								await handleResponse(
+									ws,
+									options.message?.(
+										new ElysiaWS(
+											ws,
+											context as any,
+											message
+										),
+										message as any
+									)
+								)
+							} catch (error) {
+								console.log(error)
+								handleErrors(ws, error)
+							}
+						},
+						drain: async (ws: ServerWebSocket<any>) => {
+							try {
+								await handleResponse(
+									ws,
+									options.drain?.(
+										new ElysiaWS(ws, context as any)
+									)
+								)
+							} catch (error) {
+								handleErrors(ws, error)
+							}
+						},
+						close: async (
+							ws: ServerWebSocket<any>,
+							code: number,
+							reason: string
+						) => {
+							try {
+								await handleResponse(
+									ws,
+									options.close?.(
+										new ElysiaWS(ws, context as any),
+										code,
+										reason
+									)
+								)
+							} catch (error) {
+								handleErrors(ws, error)
+							}
+						}
+					}
+
+					return ''
+				},
+				{
+					...rest,
+					websocket: options
+				} as any
+			)
+		},
 		listen(app) {
 			return (options, callback) => {
 				if (typeof options === 'string') {
@@ -19,22 +257,55 @@ export const node = () => {
 					options = parseInt(options)
 				}
 
+				const websocket = defineHooks({
+					async upgrade(request) {
+						// @ts-ignore
+						const id = (request.id = randomId())
+
+						const response = await app.handle(request)
+						if (response.status >= 300) return response
+
+						const ws = wsState[id]
+
+						return {
+							// @ts-ignore
+							headers: ws!.headers,
+							context: ws as any
+						}
+					},
+					open(ws) {
+						// @ts-ignore
+						ws.context.open?.(ws)
+					},
+					message(ws, message) {
+						// @ts-ignore
+						ws.context.message?.(ws, message)
+					}
+				})
+
 				const serverOptions =
 					typeof options === 'number'
 						? {
 								port: options,
+								websocket,
 								fetch: app.fetch
 							}
 						: {
 								...options,
-								// @ts-ignore
-								host: options?.hostname
+								websocket,
+								fetch: app.fetch
 							}
 
 				let server = serve(serverOptions)
-				const nodeServer = server.node?.server
-
-				console.log(nodeServer)
+				const nodeServer = server.node?.server as
+					| NodeServer<typeof IncomingMessage, typeof ServerResponse>
+					| Http2Server<
+							typeof IncomingMessage,
+							typeof ServerResponse,
+							typeof Http2ServerRequest,
+							typeof Http2ServerResponse
+					  >
+					| undefined
 
 				// @ts-ignore
 				const hostname = server.serveOptions.host ?? 'localhost'
@@ -100,18 +371,6 @@ export const node = () => {
 				} satisfies Server
 
 				if (callback) callback(serverInfo)
-
-				app.modules.then(() => {
-					try {
-						serverInfo.reload(
-							typeof options === 'object'
-								? (options as any)
-								: {
-										port: options
-									}
-						)
-					} catch {}
-				})
 
 				// @ts-ignore private property
 				app.router.http.build?.()
