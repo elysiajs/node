0aae1414853b9e62eca937bc6f9a3c81874f1e27
saltyaom <saltyaom@gmail.com>
2026-05-20 04:26:26 +0700

    :tada: feat: use elysia namespace

--- example/index.ts
+++ example/index.ts
@@ -4,35 +4,12 @@ import { fromTypes, openapi } from '@elysiajs/openapi'
 
 import { node } from '../src'
 
+const subapp = new Elysia({ adapter: node() }).ws('/', {
+	message: () => 'Hello WebSocket'
+})
+
 const app = new Elysia({
 	adapter: node()
 })
-
-app.use(cors())
-	.use(openapi())
-	.get('/redirect', ({ cookie, redirect }) => {
-		cookie.a.value = 'cookie value'
-
-		return redirect('https://example.com')
-	})
-	.ws('/ws/:id', {
-		open({ data, subscribe, isSubscribed }) {
-			console.log(data)
-			subscribe('welcome')
-		},
-		message(ws, message) {
-			ws.send(message)
-		}
-	})
-	.get('/image', async () => file('test/kyuukurarin.mp4'))
-	.get('/generator', async function* () {
-		for (let i = 0; i < 10000; i++) {
-			await new Promise((resolve) => setTimeout(resolve, 10))
-			yield 'A'
-		}
-	})
-	.post('/', ({ body }) => body, {
-		parse: 'json'
-	})
-	.get('/', () => 'ok')
+	.use(subapp)
 	.listen(3000)
